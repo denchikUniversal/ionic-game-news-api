299dd7868ea548990ed5293d9dbd338d817cf1f4
denchikUniversal <tkachenkodenys94@gmail.com>
2018-08-14 16:34:39 +0300

    fix2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items:any = [];
 
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+    this.getData();
+  }
+
+  getData(){
+    let data: Observable<any>;
+    data = this.http.get('https://newsapi.org/v2/top-headlines?sources=ign&apiKey=4370ceb1a6fe4e1ba693a36ce793609b');
+
+    data.subscribe(result => {
+      this.items = result.articles;
+      console.log(result.articles);
+    })
   }
 
 }
